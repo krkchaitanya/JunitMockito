6865b8556f1261ba5b0b3c21b9ef4fce0df3a3f8
krk chaitanya <krkchaitanya42@gmail.com>
2018-06-08 23:15:27 -0600

    Initial Commmit

---
+++ src/main/java/com/junitWork/testCases/TestCasesApplication.java
@@ -0,0 +1,12 @@
+package com.junitWork.testCases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCasesApplication.class, args);
+	}
+}
