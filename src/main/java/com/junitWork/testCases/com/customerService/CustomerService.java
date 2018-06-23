5d1a42063fdb2eb672a5af0b440489d3cdc10296
krk chaitanya <krkchaitanya42@gmail.com>
2018-06-22 22:23:06 -0600

    CustomerDAO

--- src/main/java/com/junitWork/testCases/com/customerService/CustomerService.java
+++ src/main/java/com/junitWork/testCases/com/customerService/CustomerService.java
@@ -1,6 +1,6 @@
 package com.junitWork.testCases.com.customerService;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customerService")
 public class CustomerService {
 
+    @Autowired
+    private CustomerDao customerDao;
 
+    public CustomerDao getCustomerDetails(){
+        CustomerDao customerInfo=new CustomerDao("mikel","mieklle@email.com",22);
+        return customerInfo;
+    }
 }
+
+
+
